1cc8af974856dbebf43abf4f59454acece04b3f5
Mitt <mittcoats@gmail.com>
2016-03-09 06:30:47 +0000

    add nested form functionality

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,10 +1,15 @@
 class Hotel < ActiveRecord::Base
   has_many :room_categories, dependent: :destroy
   
+  accepts_nested_attributes_for :room_categories, 
+                                reject_if: proc { |attributes| attributes['name'].blank? },
+                                allow_destroy: true
+  
   validates :name,
             presence: true
             
   def to_s
     name
   end
+  
 end
